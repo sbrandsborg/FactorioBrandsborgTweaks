00540eeef566262009895985dce631732837da29
sbrandsborg <steffen@sbrandsborg.dk>
2017-12-18 02:36:43 +0100

    Create settings.lua

---
+++ Brandsborg_Tweaks_0.0.1/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "BT-Player_Inventory_Size",
+		setting_type = "startup",
+		default_value = 500,
+		minimum_value = 1,
+		maximum_value = 2500,
+	},
+})
